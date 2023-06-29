2e59993af7c8955239a3380e1b44fe6b1ef16bec
eqxonee <lich1997a@yandex.ru>
2023-06-29 20:46:13 +0300

    Initial commit

---
+++ src/main/java/com/example/barhatnayatyaga/BarhatnayaTyagaApplication.java
@@ -0,0 +1,13 @@
+package com.example.barhatnayatyaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarhatnayaTyagaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarhatnayaTyagaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/barhatnayatyaga/BarhatnayaTyagaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.barhatnayatyaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarhatnayaTyagaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
